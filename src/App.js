a3c420e388fef21dbf261189713e8522a0e7d7a0
Mike Tsakiroglou <32255755+MikeTsak@users.noreply.github.com>
2024-09-25 15:27:02 +0300

    Filished intrrcation page v3

--- src/App.js
+++ src/App.js
@@ -27,24 +27,6 @@ function App() {
     <div className="App">
       {!startClicked ? (
         <>
-          {/* Guitar Pick Background as inline style */}
-          <div
-            className="guitar-pick-background"
-            style={{
-              position: 'absolute',
-              zIndex: 1,
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              backgroundImage: `url(${process.env.PUBLIC_URL + '/pick.svg'})`, // Correctly reference the SVG from public folder
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: '50vw 50vw',
-              opacity: 0.1,
-              width: '50vw',
-              height: '50vw',
-            }}
-          ></div>
-
           <header className="header">
             <h1>Michail Tsakiroglou</h1>
             <h2>Software Engineer</h2>
@@ -66,28 +48,27 @@ function App() {
 
           <div className="contact-info">
             <div className="contact-item">
-              <FontAwesomeIcon icon={faPhone} />
-              <a href="tel:+302104441096"> +30 210 444 1096</a>
+              <FontAwesomeIcon icon={faPhone} /> 
+              <a href="tel:+302104441096">+30 210 444 1096</a> {/* Clickable phone link */}
             </div>
             <div className="contact-item">
-              <FontAwesomeIcon icon={faEnvelope} />
-              <a href="mailto:hello@miketsak.gr"> hello@miketsak.gr</a>
+              <FontAwesomeIcon icon={faEnvelope} /> 
+              <a href="mailto:hello@miketsak.gr">hello@miketsak.gr</a> {/* Clickable email link */}
             </div>
             <div className="contact-item">
-              <FontAwesomeIcon icon={faMapMarkerAlt} />
-              <a href="https://maps.app.goo.gl/RR8Xcih5PhQeQcAv9" target="_blank" rel="noopener noreferrer">
-                Athens, Greece
-              </a>
+              <FontAwesomeIcon icon={faMapMarkerAlt} /> 
+              <a href="https://maps.app.goo.gl/RR8Xcih5PhQeQcAv9" target="_blank" rel="noopener noreferrer">Athens, Greece
+              </a> {/* Clickable address link */}
             </div>
             <div className="social-links">
               <a href="https://github.com/MikeTsak" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithub} /> GitHub
+                <FontAwesomeIcon icon={faGithub} />GitHub
               </a>
               <a href="https://www.linkedin.com/in/michail-tsakiroglou/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+                <FontAwesomeIcon icon={faLinkedin} />LinkedIn
               </a>
               <a href="https://agileadvisors.gr/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faBriefcase} /> Agile Advisors
+                <FontAwesomeIcon icon={faBriefcase} />Agile Advisors
               </a>
             </div>
           </div>
